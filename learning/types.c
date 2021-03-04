4886dbf9ffaaf44e4fccaa553c71730ab2f0a29e
Thomas Sobor <sobor.thomas@yahoo.com>
2021-03-03 19:30:19 -0500

    begin c data types

---
+++ learning/types.c
@@ -0,0 +1,18 @@
+/*
+The C language has the following data types:
+        TYPE                STORAGE SIZE        VALUE RANGE
+
+        char                1 byte              -128 to 127 or 0 to 255
+        unsigned char       1 byte              0 to 255
+        signed char         1 byte              -128 to 127
+        int                 2 or 4 bytes        -32,768 to 32,767 or -2,147,483,648 to 2,147,483,647
+        unsigned int        2 or 4 bytes        0 to 65,535 or 0 to 4,294,967,295
+        short               2 bytes             -32,768 to 32,767
+        unsigned short      2 bytes             0 to 65,535
+        long                8 bytes or          -9223372036854775808 to 9223372036854775807
+                            4 bytes for
+                            32 bit OS
+        unsigned long       8 bytes             0 to 18446744073709551615
+
+        
+*/
