239d17161977a14512061a36040e9cd17244a1a4
Thomas Sobor <sobor.thomas@yahoo.com>
2021-03-05 16:21:16 -0500

    start on loops

---
+++ learning/loops.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main()
+{
+    // while loop
+    int i = 0;
+    while (i < 10)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+
+    // for loop
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", i);
+    }
+}
