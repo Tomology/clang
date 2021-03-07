2a5eef21d69d918b9f5b6642702b007ac0c36664
Thomas Sobor <sobor.thomas@yahoo.com>
2021-03-07 12:39:14 -0500

    add format specifiers

---
+++ learning/format_specifiers.c
@@ -0,0 +1,27 @@
+/*
+
+        FORMAT SPECIFIER            TYPE
+
+        %c                          Character
+        %d                          Signed integer
+        %e or %E                    Scientific notation of floats
+        %f                          Float values
+        %g or %G                    Similar as %e or %E
+        %hi                         Signed integer (short)
+        %hu                         Unsigned integer (short)
+        %i                          Unsigned integer
+        %l or %ld or %li            Long
+        %lf                         Double
+        %Lf                         Long Double
+        %lu                         Unsigned int or unsigned long
+        %lli or %lld                Long long
+        %llu                        Unsigned long long
+        %o                          Octal representation
+        %p                          Pointer
+        %s                          String
+        %u                          Unsigned int
+        %x or %X                    Hexadecimal representation
+        %n                          Prints nothing
+        %%                          Prints % character
+
+*/
