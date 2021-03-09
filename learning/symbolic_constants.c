c7c28dcae1260c0d8455ad9e22d919e7aa3e8c17
Thomas Sobor <sobor.thomas@yahoo.com>
2021-03-08 19:36:50 -0500

    add symbolic constants

---
+++ learning/symbolic_constants.c
@@ -0,0 +1,9 @@
+/*
+A #define line defines a symbolic name or symbolic constant. 
+A symbolic constant is a name given to some numeric, character, or string constant.
+Symbolic constant names are written in uppercase by convention, but it is not required.
+*/
+
+#define SYMBOLIC_CONSTANT_NAME 'VALUE OF THE CONSTANT'
+#define PI 3.14
+#define MAX 100
