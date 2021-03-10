05e65e454e17e185f6a046522a95f1f066c2c058
Thomas Sobor <sobor.thomas@yahoo.com>
2021-03-09 19:11:56 -0500

    add escape sequences

---
+++ learning/escape_sequences.c
@@ -0,0 +1,15 @@
+/*
+    ESCAPE SEQUENCES        CHARACTER
+
+    \b                      Backspace
+    \f                      Form feed
+    \n                      Newline
+    \r                      Return
+    \t                      Horizontal tab
+    \v                      Vertical tab
+    \\                      Backslash
+    \'                      Single quotation mark
+    \"                      Double quatation mark
+    \?                      Question mark
+    \0                      Null character
+*/
