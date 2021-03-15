f47baa391d294f98463c353d785096279c609520
Thomas Sobor <sobor.thomas@yahoo.com>
2021-03-15 19:25:07 -0400

    update loops

--- learning/loops.c
+++ learning/loops.c
@@ -10,6 +10,14 @@ main()
         i++;
     }
 
+    // do while loop
+    int i = 0;
+    do
+    {
+        printf("%d\n", i);
+        i++;
+    } while (i < 0);
+
     // for loop
     for (int i = 0; i < 10; i++)
     {
