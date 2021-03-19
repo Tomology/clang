542dcaab3fe2f0b1a017f5dc55b87639cd63558e
Thomas Sobor <sobor.thomas@yahoo.com>
2021-03-18 20:25:37 -0400

    update storage classes

--- learning/storage_classes.c
+++ learning/storage_classes.c
@@ -24,4 +24,16 @@
     They can be accessed outside their scope as well using the concept of pointers, by poiting to the exact memory location
     where the variable resides. They are assigned a garbage value by default whenever they are declared.
 
+    2) EXTERN
+
+    Extern storage class simply tells us that the variable is defined elsewhere and within the same block where it is used. Basically,
+    the value is assigned to it in a different block and this can be overwritten/changed in a different block as well. An extern 
+    variable is a global variable initialized with a legal value where it is declared in order to be used elsewhere. It can be
+    accessed within any function/block. Also, a normal global variable can be made extern as well by placing the 'extern' keyword
+    before its declaration/definition in any function/block. This basically signifies that we are not initializing a new variable
+    but instead we are using/accessing the global variable only. The main purpose of using extern variables is that they can be
+    accessed between two different files which are part of a large program. 
+
+    
+
 */
