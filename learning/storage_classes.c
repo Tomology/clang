bd603cd944992a2fc656ea95fecc65a2c747536d
Thomas Sobor <sobor.thomas@yahoo.com>
2021-03-19 09:15:52 -0400

    add static storage class explanation

--- learning/storage_classes.c
+++ learning/storage_classes.c
@@ -34,6 +34,14 @@
     but instead we are using/accessing the global variable only. The main purpose of using extern variables is that they can be
     accessed between two different files which are part of a large program. 
 
+    3) STATIC
+
+    The static storage class instructs the compiler to keep a local variable in existence during the life-time of the program instead
+    of creating and destroying it each time it comes into and goes out of scope. Therefore, making local variables static allows them 
+    to maintain their values between function calls. By default, they are assigned the value 0 by the compiler.
+    The static modifier may also be applied to global variables. When this is done, it causes that variable's scope to be restricted to 
+    the file in which it is declared.
+
     
 
 */
